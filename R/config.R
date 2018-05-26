3957a67b557436be4783b563c30c21a879a2179b
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-05-26 17:37:30 +0200

    package init

---
+++ R/config.R
@@ -0,0 +1,12 @@
+sequilaEnv <- new.env()
+sequilaEnv$version <- "0.4-SNAPSHOT"
+sequilaEnv$sparkVersion <- "2.2.1"
+sequilaEnv$config <- sparklyr::spark_config()
+sequilaEnv$config$sparklyr.defaultPackages <- paste("org.biodatageeks","bdg-sequila_2.11",sequilaEnv$version,sep=":")
+sequilaEnv$config$sparklyr.shell.repositories <-"https://zsibio.ii.pw.edu.pl/nexus/repository/maven-snapshots/,https://zsibio.ii.pw.edu.pl/nexus/repository/maven-releases/"
+
+.onLoad <- function(libname, pkgname){
+  library(sparklyr)
+  spark_install(version=sequilaEnv$sparkVersion)
+  packageStartupMessage(paste("SeQuiLa-sparklyr",sequilaEnv$version, sep=" "))
+}
