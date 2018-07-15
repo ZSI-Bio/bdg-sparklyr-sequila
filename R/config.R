fdbcd80323dc3974f1d7f25832cb27406d7f49af
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-07-15 09:21:06 +0200

    bumped to spark 2.2.2 (#8)

--- R/config.R
+++ R/config.R
@@ -1,6 +1,6 @@
 sequilaEnv <- new.env()
 sequilaEnv$version <- "0.4-SNAPSHOT"
-sequilaEnv$sparkVersion <- "2.2.1"
+sequilaEnv$sparkVersion <- "2.2.2"
 sequilaEnv$config <- sparklyr::spark_config()
 sequilaEnv$config$sparklyr.defaultPackages <- paste("org.biodatageeks","bdg-sequila_2.11",sequilaEnv$version,sep=":")
 sequilaEnv$config$sparklyr.shell.repositories <-"https://zsibio.ii.pw.edu.pl/nexus/repository/maven-snapshots/,https://zsibio.ii.pw.edu.pl/nexus/repository/maven-releases/"
